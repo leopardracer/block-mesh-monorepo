b7a47496cc066b1d1f33ed3fd748bd5a2cb13436
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-04-19 20:23:14 +0300

    Update select_app.rs

--- libs/blockmesh-bin/src/pages/apps/select_app.rs
+++ libs/blockmesh-bin/src/pages/apps/select_app.rs
@@ -8,7 +8,7 @@ pub fn SelectApps() -> impl IntoView {
         <section class="p-8 lg:py-20">
             <div class="container !mx-auto text-center place-content-center grid">
                 <p class="mb-2 block antialiased font-sans leading-relaxed text-blue-gray-900 !font-semibold lg:!text-lg !text-base">
-                    More DePIN oppurtunities
+                    More DePIN opportunities
                 </p>
                 <div class="flex flex-col md:flex-row gap-6 max-w-6xl mx-auto">
                     <div class="flex flex-col items-center justify-center gap-6">
